d554729a4a371b395f348f026c9c5c9f44a1d478
Jorge Cancino <69229690+Jorgefitss@users.noreply.github.com>
2023-09-27 19:07:53 -0500

    Update Reto2.sql

--- Reto2.sql
+++ Reto2.sql
@@ -1,34 +1,46 @@
+/*RETO2*/
 describe Clientes
 
 --1  En la tabla de Clientes agregar los atributos:
 ALTER TABLE Clientes
 Add NOM_CORTO Varchar2(100) add EDAD Number(3,0);
 
+
 --2  Actualizar el atributo NOM_CORTO con el contenido de: VAL_NOM1 y VAL_APE1
---  En la actualización se debe de considerar que la primera letra de cada atributo se conserve en Mayúscula y el resto en minúscula
- 
+--  En la actualización se debe de considerar que la primera letra de cada atributo se conserve en Mayúscula y el resto en minúsculas
+
+BEGIN
 UPDATE Clientes
 SET NOM_CORTO = INITCAP(VAL_NOM1) || ' ' || INITCAP(VAL_APE1);
+    For Nom IN(Select NOM_CORTO from clientes) Loop
+    dbms_output.put_line('NOM_CORTO: ' ||  Nom.NOM_CORTO);
+    END Loop;
+END;
 
-Select * from clientes;
+ 
+ --3  Calcular la edad de cada cliente y actualizar el atributo EDAD.
 
---3  Calcular la edad de cada cliente y actualizar el atributo EDAD.
+BEGIN
 UPDATE Clientes
-SET Edad = trunc((sysdate - FEC_NACI)) /365;
+SET Edad = TRUNC(MONTHS_BETWEEN(SYSDATE, FEC_NACI) / 12, 2);
+    For Edad_x IN(Select Edad from clientes) Loop
+    dbms_output.put_line('EDAD: ' || Edad_x.Edad);
+    END Loop;
+END;
 
-Select edad from clientes;
 
---4  Generar una sentencia que reemplace la letra “Ñ” por la letra “N” en los atributos VAL_APE1 y VAL_APE2 de la tabla Clientes
-Update Clientes
-Set VAL_APE1 = Replace(VAL_APE1,'Ñ','N'),
-    VAL_APE2 = Replace(VAL_APE2,'Ñ','N');
-    commit;
 
-Select VAL_APE1, VAL_APE2 from Clientes 
-where VAL_APE1 || VAL_APE2 Like '%N%'; 
+ --4  Generar una sentencia que reemplace la letra “Ñ” por la letra “N” en los atributos VAL_APE1 y VAL_APE2 de la tabla Clientes
 
-Select VAL_APE1, VAL_APE2 from Clientes 
-where VAL_APE1 || VAL_APE2 Like '%Ñ%'; 
+DECLARE
+V_REPLACE Varchar2(30) := 'Ñ';
+BEGIN
+UPDATE clientes
+Set VAL_APE1 = Replace(VAL_APE1, V_REPLACE, 'N'),
+    VAL_APE2 = Replace(VAL_APE2, V_REPLACE, 'N');
+ For cliente_x IN (Select * from clientes where VAL_APE1 || VAL_APE2 LIKE '%N%') Loop
+ dbms_output.put_line('VAL_APE1: ' || cliente_x.VAL_APE1 || ', VAL_APE2: ' || cliente_x.VAL_APE2);
+    END Loop;
+END;
 
-SELECT VAL_APE1 from clientes where VAL_APE1= 'TITUANA';
-
+ 
